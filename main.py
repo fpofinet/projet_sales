01a4f6d8c897f898978361ff23f5a0f78a9f054f
ragellan-dev <lionel@placholder.com>
2026-06-16 15:52:07 +0000

    finalisation de l'extractor des taux de change

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pandas as pd
+import logging
+import src.extractor as ex
+
+# Configuration des logs (écriture dans un fichier + affichage console)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("pipeline.log"),
+        logging.StreamHandler()
+    ]
+)
 
 def run_pipeline():
     """pipeline principale"""
     raw_trans = pd.read_csv('data/transactions.csv')
-    print(raw_trans.info())
+    unique_date = raw_trans["date"].unique()
+    #recuperation des taux de changes pour chaque date
+    rates = ex.retrieveExchangeRate(unique_date)
+    print(rates)
 
 if __name__ == "__main__":
     run_pipeline()
---
+++ src/extractor.py
@@ -0,0 +1,22 @@
+import requests
+import logging
+
+
+def retrieveExchangeRate(dates):
+    """Cette fonction nous permet de recuperer les taux de changes
+        d'une date. elle renvoie un dictionnaire avec pour chaque 
+        date la liste des taux de change
+    """
+
+    rates =dict()
+    for date in dates:
+        try :
+            response = requests.get(url=f"https://api.frankfurter.dev/v2/rates?date={date}")
+            response.raise_for_status()
+            logging.info(f"Recuperation des taux pour la date {date}")
+            rates[date]=response.json()
+        except requests.exceptions.RequestException as e:
+            logging.warning(f"Echec de recuperation des taux pour la date {date}")
+            return None
+    
+    return rates
